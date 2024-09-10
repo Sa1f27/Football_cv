92baf0e1faf440b01011778a7f895f8bbc9a6edf
Huzaifah <154003597+Sa1f27@users.noreply.github.com>
2024-09-10 23:59:03 +0530

    Update football_analysis.py

--- football_analysis.py
+++ football_analysis.py
@@ -5,5 +5,4 @@ import numpy as np
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
-from view_transformer import ViewTransformer
 from speed_and_distance_estimator import SpeedAndDistance_Estimator
