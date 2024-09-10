a57576d71da700a804dfdc6b931d51afecee776a
Huzaifah <154003597+Sa1f27@users.noreply.github.com>
2024-09-10 23:58:45 +0530

    Update yolo_inference.py

--- yolo_inference.py
+++ yolo_inference.py
@@ -4,6 +4,5 @@ model = YOLO('models/best.pt')
 
 results = model.predict('input_videos/08fd33_4.mp4',save=True)
 print(results[0])
-print('=====================================')
 for box in results[0].boxes:
     print(box)
