06921295380681d74ea1a91ed8c0d39ec65ceaa7
Huzaifah <154003597+Sa1f27@users.noreply.github.com>
2024-10-07 23:24:30 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -193,5 +193,26 @@ class Tracker:
             referee_dict = tracks["referees"][frame_num]
 
             # Draw Players
+            for track_id, player in player_dict.items():
+                color = player.get("team_color",(0,0,255))
+                frame = self.draw_ellipse(frame, player["bbox"],color, track_id)
+
+                if player.get('has_ball',False):
+                    frame = self.draw_traingle(frame, player["bbox"],(0,0,255))
+
+            # Draw Referee
+            for _, referee in referee_dict.items():
+                frame = self.draw_ellipse(frame, referee["bbox"],(0,255,255))
             
+            # Draw ball 
+            for track_id, ball in ball_dict.items():
+                frame = self.draw_traingle(frame, ball["bbox"],(0,255,0))
+
+
+            # Draw Team Ball Control
+            frame = self.draw_team_ball_control(frame, frame_num, team_ball_control)
+
+            output_video_frames.append(frame)
+
+        return output_video_frames
     
